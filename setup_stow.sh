ef2c0b63e46f9a5cbdcca575d8f69fdbdeb1427e
nfrith <nf@s9t.dev>
2024-05-04 18:30:41 +0700

    stow

--- setup_stow.sh
+++ setup_stow.sh
@@ -1,12 +1,26 @@
-rm -rf ~/.config/nvim
-rm ~/.bashrc
-rm ~/.zshrc
-rm ~/.wezterm.lua
-
-if [ ! -d ~/.config/nvim ]; then
-    sudo mkdir ~/.config/nvim
+if [ -d ~/.config/nvim ]; then
+    echo 'removing ~/.config/nvim'
+    sudo rm -rf ~/.config/nvim
 fi
 
+if [ -f ~/.bashrc ]; then
+    echo 'removing ~/.bashrc'
+    sudo rm ~/.bashrc
+fi
+
+if [ -f ~/.zshrc ]; then
+    echo 'removing ~/.zshrc'
+    sudo rm ~/.zshrc
+fi
+
+if [ -f ~/.wezterm.lua ]; then
+    echo 'removing ~/.wezterm.lua'
+    sudo rm ~/.wezterm.lua
+fi
+
+
+echo 'applying stow'
 stow bash
 stow wezterm
 sudo stow -t ~/.config/nvim neovim
+echo 'done applying stow'
