8519e12e9be9fdffbb088f1ac15df44c623b4da8
nfrith <nf@s9t.dev>
2024-05-04 18:18:34 +0700

    update stow setup

--- setup_stow.sh
+++ setup_stow.sh
@@ -1,5 +1,12 @@
+rm -rf ~/.config/nvim
+rm ~/.bashrc
+rm ~/.zshrc
+rm ~/.wezterm.lua
+
 if [ ! -d ~/.config/nvim ]; then
     sudo mkdir ~/.config/nvim
 fi
 
+stow bash
+stow wezterm
 sudo stow -t ~/.config/nvim neovim
