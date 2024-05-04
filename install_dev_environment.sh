dfe85680a9a755609dba15f190a819ae70507af1
nfrith <nf@s9t.dev>
2024-05-04 20:06:11 +0700

    wezterm config add gpu

---
+++ install_dev_environment.sh
@@ -0,0 +1,28 @@
+# The context of this script is you are in a machine that has source code to be directly worked on
+sudo apt-get update
+sudo apt-get install ripgrep
+sudo apt-get install fd-find
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+
+# Check for modification in .bashrc
+if ! grep -qF 'export PATH=$PATH:/opt/nvim-linux64/bin' ~/.bashrc; then
+    echo "Configuring neovim to path in bashrc..."
+    echo 'export PATH=$PATH:/opt/nvim-linux64/bin' >> ~/.bashrc
+else
+    echo 'Skipping PATH modification for neovim in ~/.bashrc as it already exists on system...'
+fi
+
+# Check for modification in .zshrc
+if ! grep -qF 'export PATH=$PATH:/opt/nvim-linux64/bin' ~/.zshrc; then
+    echo "Configuring neovim to path in zshrc..."
+    echo 'export PATH=$PATH:/opt/nvim-linux64/bin' >> ~/.zshrc
+else
+    echo 'Skipping PATH modification for neovim in ~/.zshrc as it already exists on system...'
+fi
+
+sudo chmod +x ./setup_dev_stow.sh
+./setup_dev_stow.sh
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,10 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = 'AdventureTime'
+config.front_end = 'OpenGL'
+config.color_scheme = 'Batman'
+config.font = wezterm.font("JetBrainsMono NF")
+config.font_size = 12
 config.default_domain = 'WSL:Ubuntu'
 config.keys = {
   {
